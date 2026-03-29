ca6262c3940f3c9eb472ded160813d3544bcadc2
yousif <yousif.sei07@gmail.com>
2026-03-29 13:18:42 +0300

    add EnableAsync to main application class

--- src/main/java/com/ga/processor/concurrent_csv_processor/ConcurrentCsvProcessorApplication.java
+++ src/main/java/com/ga/processor/concurrent_csv_processor/ConcurrentCsvProcessorApplication.java
@@ -2,8 +2,10 @@ package com.ga.processor.concurrent_csv_processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ConcurrentCsvProcessorApplication {
 
 	public static void main(String[] args) {
